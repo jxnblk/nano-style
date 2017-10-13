bce2c611ab3e6952c0b6e02fe69b8a3790d5ed65
Brent Jackson <jxnblk@gmail.com>
2017-10-12 20:24:40 -0400

    Test state based styles

--- src/index.js
+++ src/index.js
@@ -11,26 +11,58 @@ const styled = Component => (...args) => {
   const dynamicStyles = args.filter(a => typeof a === 'function')
   const baseClassName = prefix + hash(JSON.stringify(staticStyles))
   const base = parse('.' + baseClassName, staticStyles)
+  const isElement = typeof Component === 'string'
 
   class Styled extends React.Component {
-    render () {
-      const styles = dynamicStyles
-        .map((func, i) => func(this.props))
+    constructor (props) {
+      super(props)
 
-      const dynamicClassName = prefix + hash(JSON.stringify(styles))
-      const css = styles.map(style => parse('.' + dynamicClassName, style)).join('')
+      this.getStyles = props => {
+        const styles = dynamicStyles.map(fn => fn(props))
+        const className = prefix + hash(JSON.stringify(styles))
+        const css = styles.map(style => parse('.' + className, style)).join('')
 
-      const blacklist = Object.keys(ThemeStyled.propTypes || {})
-      const next = {}
-      for (let key in this.props) {
-        if (typeof Component === 'string' && blacklist.includes(key)) continue
-        next[key] = this.props[key]
+        this.setState({
+          className,
+          css
+        })
       }
 
+      this.getProps = props => {
+        if (!isElement) return props
+        const next = {}
+        const blacklist = Object.keys(ThemeStyled.propTypes || {})
+        for (let key in props) {
+          if (blacklist.includes(key)) continue
+          next[key] = props[key]
+        }
+        return next
+      }
+
+      this.state = {
+        className: '',
+        css: ''
+      }
+    }
+
+    componentWillMount () {
+      this.getStyles(this.props)
+    }
+
+    componentWillReceiveProps (next) {
+      if (next !== this.props) {
+        this.getStyles(next)
+      }
+    }
+
+    render () {
+      const { className, css } = this.state
+      const next = this.getProps(this.props)
+
       next.className = [
-        this.props.className,
+        next.className,
         baseClassName,
-        dynamicClassName
+        className
       ].join(' ').trim()
 
       return [
